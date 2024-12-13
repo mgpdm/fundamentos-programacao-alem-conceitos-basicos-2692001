38e6dc3461a55693da98004776b7e901d235511a
mgpdm <167898942+mgpdm@users.noreply.github.com>
2024-12-13 18:37:33 +0000

    Ok

--- Unidad_04/04_04/inicio/app.py
+++ Unidad_04/04_04/inicio/app.py
@@ -1,2 +1,7 @@
 radio = input('Ingrese el valor del radio del círculo: ')
 # area_circulo = (valor_radio ** 2) * 3.14159
+
+raio_float = float(radio)
+area = (2*3.14159)*raio_float
+print('A área do círculo é:')
+print(area)
