0ae4cf1c6488c3b1695ca0f4a8420d950433398f
mgpdm <167898942+mgpdm@users.noreply.github.com>
2024-12-13 15:26:51 +0000

    Feito

--- Unidad_01/01_06/inicio/colecciones.py
+++ Unidad_01/01_06/inicio/colecciones.py
@@ -1,14 +1,28 @@
 # Estrellas más cercanas a la Tierra
-estrella1 = 'Sol'
-estrella2 = 'Alfa Centauri'
-estrella3 = 'Estrella de Barnard'
-estrella4 = 'Luhman 16'
+# estrella1 = 'Sol'
+# estrella2 = 'Alfa Centauri'
+# estrella3 = 'Estrella de Barnard'
+# estrella4 = 'Luhman 16'
+
+estrelas = ['Sol','Alfa Centauri','Estrella de Barnard','Luhman 16']
+print(estrelas[3])
 
 # Montañas más altas en cada placa tectónica:
-Africana = 'Kilimanjaro'
-Antartica = 'Macizo Vinson'
-Indoaustraliana = 'Monte Jaya'
-Euroasiatica = 'Everest'
-Norteamerica = 'Monte Denali'
-Pacifico = 'Volcán Mauna Kea'
-Suramericana = 'Aconcagua'
+#Africana = 'Kilimanjaro'
+#Antartica = 'Macizo Vinson'
+#Indoaustraliana = 'Monte Jaya'
+#Euroasiatica = 'Everest'
+#Norteamerica = 'Monte Denali'
+#Pacifico = 'Volcán Mauna Kea'
+#Suramericana = 'Aconcagua'
+
+montanhas = {
+    'Africana':'Kilimanjaro',
+    'Antartica':'Macizo Vinson',
+    'Indoaustraliana':'Monte Jaya',
+    'Euroasiatica':'Everest',
+    'Norteamerica':'Monte Denali',
+    'Pacifico':'Volcán Mauna Kea',
+    'Suramericana':'Aconcagua'}
+
+print(montanhas['Pacifico'])
